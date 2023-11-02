b8fe5b8695adc792b2b304ad0d50447a7d17751e
abel <abelbeyene373@gmail.com>
2023-11-02 20:29:31 +0100

    added detials screen, payment method adition

--- Coffe_shop_app/src/screens/HomeScreen.tsx
+++ Coffe_shop_app/src/screens/HomeScreen.tsx
@@ -170,7 +170,11 @@ const HomeScreen = ({navigation}: any) => {
             return (
               <TouchableOpacity
                 onPress={() => {
-                  navigation.push('Details');
+                  navigation.push('Details', {
+                    index: item.index,
+                    id: item.id,
+                    type: item.type,
+                  });
                 }}>
                 <CoffeeCard
                   id={item.id}
@@ -203,7 +207,11 @@ const HomeScreen = ({navigation}: any) => {
             return (
               <TouchableOpacity
                 onPress={() => {
-                  navigation.push('Details');
+                  navigation.push('Details', {
+                    index: item.index,
+                    id: item.id,
+                    type: item.type,
+                  });
                 }}>
                 <CoffeeCard
                   id={item.id}
